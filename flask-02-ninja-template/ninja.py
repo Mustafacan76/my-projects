043ab1b9d2392fd6c06113ec8ba869d2cd4e6ba2
Mustafacan76 <mbozbayir67@gmail.com>
2022-09-07 14:47:15 +0300

    modify edildi

--- flask-02-ninja-template/ninja.py
+++ flask-02-ninja-template/ninja.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.routr('/')
+@app.route('/')
 def head():
     return render_template('index.html', number1 = 7000, number2 = 9000)
 
